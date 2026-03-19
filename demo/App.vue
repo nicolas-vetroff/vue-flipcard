15e91aa0f7917c9657f963250b3b2178dfccb4ea
Nicolas VETROFF <nvetroff@groupempsa.com>
2026-03-19 17:16:14 +0100

    fixed demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 </script>
 
 <template>
     <div>
         <h1>Vue Flip Card Example</h1>
-        <FlipCard active-drag active-click active-hover flip-side="up">
+        <VFlipCard
+            active-drag
+            active-click
+            active-hover
+            flip-side="right"
+        >
             <template #front>
                 <p>Front Side</p>
             </template>
             <template #back>
                 <p>Back Side</p>
             </template>
-        </FlipCard>
+        </VFlipCard>
     </div>
 </template>
--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueFlip from '../src' // src/index.ts
+import VFlipCard from '../src' // src/index.ts
 
 const app = createApp(App)
-app.use(VueFlip)
+app.use(VFlipCard)
 app.mount('#app')
